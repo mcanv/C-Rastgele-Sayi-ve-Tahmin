7ee22dc88bf4cdddde07557d4084dc7b500cde4b
kurth4cker <kurth4cker@tutanota.com>
2022-09-23 19:53:43 +0300

    main.c: syntax guideline change
    - using bsd style
    - removed including conio.h
    
    Signed-off-by: kurth4cker <kurth4cker@tutanota.com>

--- main.c
+++ main.c
@@ -1,38 +1,43 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
 #include <time.h>
 
-void main() {
-    int sayi,
-        tahmin,
-        baslangic = 1, 
-        bitis = 99, 
-        hak = 20;
-    srand(time(NULL));
-    printf("%d ile %d arasında bir sayı oluşturuluyor...\n", baslangic, bitis);
-    sayi = baslangic+rand()%bitis;
-    printf("Toplam %d hakkınız var\n", hak);
-    for(int i=1;i<=hak;i++) {
-        printf("Tahmin gir: ");
-        scanf("%d", &tahmin);
-        if(tahmin > bitis || tahmin < baslangic) {
-            printf("Girdiğiniz değer uygun bir sayı değil!\n");
-            break;
-        }
-        if(i == hak && tahmin != sayi) {
-            printf("Bulamadın! Sayı: %d", sayi);
-            break;
-        }
-        if(tahmin < sayi) {
-            printf("Daha büyük bir sayi girmelisin\n");
-        }
-        else if(tahmin > sayi) {
-            printf("Daha küçük bir sayı girmelisin\n");
-        }
-        else if(tahmin == sayi) {
-            printf("Bravo! %d. tahminde sonucu buldun. Sayı: %d", i, sayi);
-            break;
-        }
-    }
+int start = 1;
+int end = 9;
+
+int
+main()
+{
+	int sayi;
+        int tahmin;
+        int hak = 20;
+
+	srand(time(NULL));
+	sayi = start + rand()%end;
+	printf("%d ile %d arasında bir sayı oluşturuluyor...\n", start, end);
+	printf("Toplam %d hakkınız var\n", hak);
+
+	for (int i = 1; i <= hak; i++) {
+		printf("Tahmin gir: ");
+		scanf("%d", &tahmin);
+
+		if (tahmin > end || tahmin < start) {
+			puts("Girdiğiniz değer uygun bir sayı değil!\n");
+			return 1;
+		}
+
+		if (tahmin < sayi) {
+			puts("Daha büyük bir sayi girmelisin");
+		} else if (tahmin > sayi) {
+			puts("Daha küçük bir sayı girmelisin");
+		} else {
+			printf("Bravo! %d. tahminde sonucu buldun. Sayı: %d\n", i, sayi);
+			return 0;
+		}
+
+		if (i == hak) {
+			printf("Bulamadın! Sayı: %d\n", sayi);
+			return 1;
+		}
+	}
 }
